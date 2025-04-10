bc70d848f59853b57b828d9b355d3a613e77cfad
AnatoliyBrovko <tolikbrovko2000@gmail.com>
2025-04-10 13:33:03 +0300

    i finish

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,7 +3,7 @@ import { Navigation } from "./components/Navigation/Navigation.jsx";
 import { QrCodeGenerator } from "./components/QrGenerator/QrGen.jsx";
 import { QrCodeScanner } from "./components/QrScanner/QrCodeScanner.jsx";
 import { GenHistory } from "./components/GenHistory/GenHistory.jsx";
-// import { ScanHistory } from "./components/ScanHistory/ScanHistory.jsx";
+import { ScanHistory } from "./components/ScanHistory/ScanHistory.jsx";
 
 export const Layout = () => {
     return (
@@ -13,7 +13,7 @@ export const Layout = () => {
                 <Route path="/generator" element={<QrCodeGenerator />} />
                 <Route path="/scan" element={<QrCodeScanner />} />
                 <Route path="/genHistory" element={<GenHistory />} />
-                {/*<Route path="/scanHistory" element={<ScanHistory />} />*/}
+                <Route path="/scanHistory" element={<ScanHistory />} />
             </Routes>
         </div>
     );
